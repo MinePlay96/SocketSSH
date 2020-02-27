aace6f8cb1c2f6104b423232be0b73f11750a75c
Leons Strauß <leonstrauss99@gmail.com>
2020-02-27 07:40:16 +0100

    added user and Server Auth without validation

--- client/index.js
+++ client/index.js
@@ -1,6 +1,15 @@
-const socket = require('socket.io-client')('http://localhost');
+const debug = true;
 
-socket.on('news', (data) => {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
+const socket = require('socket.io-client')('http://localhost');
+const os = require('os');
+
+socket.on('type', () => {
+  if (debug) console.info('Recived type request', 'server');
+  socket.emit('type', 'server');
+});
+socket.on('auth', () => {
+  if (debug) console.info('Recived auth request');
+  const authData = { hostname: os.hostname() };
+  socket.emit('auth', authData);
+  if (debug) console.info('Sended auth data', authData);
 });
--- server/js/socket.js
+++ server/js/socket.js
@@ -1,16 +1,73 @@
 // TODO: make better / butify
+// TODO: add global constants
+
+// TODO: make debug global constant
+const debug = true;
+
 const socketIo = require('socket.io');
 
-function connectionHandler(socket) {
-  socket.emit('news', { hello: 'world' });
+const serverList = {};
+const users = {};
 
-  socket.on('my other event', (data) => {
-    console.log(data);
+function authUser(user) {
+  // TODO: add auth
+  return !!user;
+}
+
+function handelServer(socket) {
+  if (debug) console.info('socket is server type');
+  socket.emit('auth');
+  socket.on('auth', (server) => {
+    if (debug) console.info('server Redistert', server);
+    serverList[server.hostname] = socket;
+    Object.keys(users).forEach((username) => {
+      users[username].emit('newServer', server.hostname);
+    });
+    socket.on('disconnect', () => {
+      delete serverList[server.hostname];
+      Object.keys(users).forEach((username) => {
+        users[username].emit('diconectServer', server.hostname);
+      });
+    });
+  });
+}
+
+function handelUser(socket) {
+  if (debug) console.info('socket is user type');
+  socket.emit('auth');
+  if (debug) console.info('asking user for auth');
+  socket.on('auth', (user) => {
+    const validUser = authUser(user);
+    if (debug) console.info('user auth', validUser);
+    if (validUser) {
+      users[user.username] = socket;
+      if (debug) console.info('sending server list', Object.keys(serverList));
+      socket.emit('serverList', Object.keys(serverList));
+    }
+  });
+}
+
+function connectionHandler(socket) {
+  if (debug) console.info('socket recives connection');
+  socket.emit('type');
+  socket.on('type', (data) => {
+    if (data) {
+      if (data === 'user') {
+        handelUser(socket);
+      } else if (data === 'server') {
+        handelServer(socket);
+      } else {
+        if (debug) console.info('socket is not a valid type closing Socket');
+        socket.emit('notValid');
+        socket.close();
+      }
+    }
   });
 }
 
 function createSocket(httpApp) {
   const io = socketIo(httpApp);
+  if (debug) console.info('socket server Startet');
   io.on('connection', connectionHandler);
 }
 
